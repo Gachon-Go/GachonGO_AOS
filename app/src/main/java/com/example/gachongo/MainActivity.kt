ae318c03947be3b68894830f8f35dd6cc004fba9
moon <0917jong@gmail.com>
2023-05-01 19:03:08 +0900

    (feat) kakao login api + login ui

--- app/src/main/java/com/example/gachongo/MainActivity.kt
+++ app/src/main/java/com/example/gachongo/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.gachongo // ktlint-disable package-name
 
+import android.content.ContentValues
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.example.gachongo.login.KakaoLoginActivity
 import com.example.gachongo_aos.R
+import com.example.gachongo_aos.databinding.ActivityMainBinding
+import com.kakao.sdk.user.UserApiClient
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        // 로그인 정보 확인
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (error != null) {
+                Log.d(ContentValues.TAG, "로그인 필요")
+                startActivity(Intent(this, KakaoLoginActivity::class.java))
+            }
+            else if (tokenInfo != null) {   // 카카오 로그인이 이미 되어있으면
+                Log.d(ContentValues.TAG, "로그인 유지 성공")
+            }
+        }
     }
 }
 
