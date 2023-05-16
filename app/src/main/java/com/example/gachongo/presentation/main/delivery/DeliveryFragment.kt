552b95249d0796dbf52a66f7b1d748b31fbcd93f
moon <0917jong@gmail.com>
2023-05-16 12:26:07 +0900

    (feat) 배달 지도

--- app/src/main/java/com/example/gachongo/presentation/main/delivery/DeliveryFragment.kt
+++ app/src/main/java/com/example/gachongo/presentation/main/delivery/DeliveryFragment.kt
@@ -1,11 +1,30 @@
 package com.example.gachongo.presentation.main.delivery
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.gachongo.util.getUserLocationCheck
+import com.example.gachongo.util.saveUserLocationCheck
 import com.example.gachongo_aos.R
+import com.example.gachongo_aos.databinding.FragmentDeliveryBinding
+import net.daum.mf.map.api.MapPOIItem
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +37,101 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class DeliveryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentDeliveryBinding
+    private val ACCESS_FINE_LOCATION = 1000
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDeliveryBinding.inflate(inflater, container, false)
+
+        // 유저한테 지도 권한 받아오기
+        if (checkLocationService()) permissionCheck()
+        else Toast.makeText(requireContext(), "GPS를 켜주세요", Toast.LENGTH_SHORT).show()
+
+        return binding.root
+    }
+
+    private fun permissionCheck() {
+        val isFirstCheck = getUserLocationCheck(requireContext())
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // 권한이 없는 상태
+            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // 권한 거절
+                Log.d("위치 권한 거절", "힝")
+                val builder = AlertDialog.Builder(requireContext())
+                builder.setMessage("현재 위치를 확인하시려면 위치 권한을 허용해주세요.")
+                builder.setPositiveButton("확인") { dialog, which ->
+                    ActivityCompat.requestPermissions(requireActivity(),
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
+                }
+                builder.setNegativeButton("취소") { dialog, which ->
+
+                }
+                builder.show()
+            } else {
+                if (isFirstCheck) {
+                    // 최초 권한 요청
+                    saveUserLocationCheck(requireContext(), false)
+                    ActivityCompat.requestPermissions(requireActivity(),
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
+                } else {
+                    val builder = AlertDialog.Builder(requireContext())
+                    builder.setMessage("현재 위치를 확인하시려면 설정에서 위치 권한을 허용해주세요.")
+                    builder.setPositiveButton("설정으로 이동") { dialog, which ->
+                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:com.example.gachongo.presentation.main.delivery"))
+                        startActivity(intent)
+                    }
+                    builder.setNegativeButton("취소") { dialog, which ->
+
+                    }
+                    builder.show()
+                }
+            }
+        } else {
+            binding.deliveryMapMv.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+
+            val mgr = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val userNowLocation: Location? = mgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+            //위도 , 경도
+            val uLatitude = userNowLocation?.latitude
+            val uLongitude = userNowLocation?.longitude
+            val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!)
+
+            // 현 위치에 마커 찍기
+            val marker = MapPOIItem()
+            marker.mapPoint = uNowPosition
+            marker.markerType = MapPOIItem.MarkerType.CustomImage
+            marker.customImageResourceId = R.drawable.ic_close
+            marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
+            binding.deliveryMapMv.addPOIItem(marker)
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_delivery, container, false)
+    // 권한 요청
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == ACCESS_FINE_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(requireContext(), "위치 권한이 승인되었습니다", Toast.LENGTH_SHORT).show()
+
+            } else {
+                Toast.makeText(requireContext(), "위치 권한이 거절되었습니다", Toast.LENGTH_SHORT).show()
+
+            }
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DeliveryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DeliveryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    // GPS가 켜져있는지 확인
+    private fun checkLocationService(): Boolean {
+        val mgr = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return mgr.isProviderEnabled(LocationManager.GPS_PROVIDER)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.deliveryMapMv.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
     }
 }
