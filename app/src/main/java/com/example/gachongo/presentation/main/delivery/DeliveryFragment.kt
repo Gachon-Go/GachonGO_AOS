68933fb82180733ca0ea0af1aef4742c5e0be728
moon <0917jong@gmail.com>
2023-05-20 00:01:01 +0900

    (feat) 배달현황 네이버 지도 연결

--- app/src/main/java/com/example/gachongo/presentation/main/delivery/DeliveryFragment.kt
+++ app/src/main/java/com/example/gachongo/presentation/main/delivery/DeliveryFragment.kt
@@ -1,30 +1,11 @@
 package com.example.gachongo.presentation.main.delivery
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import com.example.gachongo.util.getUserLocationCheck
-import com.example.gachongo.util.saveUserLocationCheck
-import com.example.gachongo_aos.R
 import com.example.gachongo_aos.databinding.FragmentDeliveryBinding
-import net.daum.mf.map.api.MapPOIItem
-import net.daum.mf.map.api.MapPoint
-import net.daum.mf.map.api.MapView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -46,92 +27,7 @@ class DeliveryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDeliveryBinding.inflate(inflater, container, false)
-
-        // 유저한테 지도 권한 받아오기
-        if (checkLocationService()) permissionCheck()
-        else Toast.makeText(requireContext(), "GPS를 켜주세요", Toast.LENGTH_SHORT).show()
-
         return binding.root
     }
 
-    private fun permissionCheck() {
-        val isFirstCheck = getUserLocationCheck(requireContext())
-        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // 권한이 없는 상태
-            if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
-                // 권한 거절
-                Log.d("위치 권한 거절", "힝")
-                val builder = AlertDialog.Builder(requireContext())
-                builder.setMessage("현재 위치를 확인하시려면 위치 권한을 허용해주세요.")
-                builder.setPositiveButton("확인") { dialog, which ->
-                    ActivityCompat.requestPermissions(requireActivity(),
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
-                }
-                builder.setNegativeButton("취소") { dialog, which ->
-
-                }
-                builder.show()
-            } else {
-                if (isFirstCheck) {
-                    // 최초 권한 요청
-                    saveUserLocationCheck(requireContext(), false)
-                    ActivityCompat.requestPermissions(requireActivity(),
-                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), ACCESS_FINE_LOCATION)
-                } else {
-                    val builder = AlertDialog.Builder(requireContext())
-                    builder.setMessage("현재 위치를 확인하시려면 설정에서 위치 권한을 허용해주세요.")
-                    builder.setPositiveButton("설정으로 이동") { dialog, which ->
-                        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse("package:com.example.gachongo.presentation.main.delivery"))
-                        startActivity(intent)
-                    }
-                    builder.setNegativeButton("취소") { dialog, which ->
-
-                    }
-                    builder.show()
-                }
-            }
-        } else {
-            binding.deliveryMapMv.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
-
-            val mgr = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-            val userNowLocation: Location? = mgr.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
-            //위도 , 경도
-            val uLatitude = userNowLocation?.latitude
-            val uLongitude = userNowLocation?.longitude
-            val uNowPosition = MapPoint.mapPointWithGeoCoord(uLatitude!!, uLongitude!!)
-
-            // 현 위치에 마커 찍기
-            val marker = MapPOIItem()
-            marker.mapPoint = uNowPosition
-            marker.markerType = MapPOIItem.MarkerType.CustomImage
-            marker.customImageResourceId = R.drawable.ic_close
-            marker.selectedMarkerType = MapPOIItem.MarkerType.RedPin
-            binding.deliveryMapMv.addPOIItem(marker)
-        }
-    }
-
-    // 권한 요청
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == ACCESS_FINE_LOCATION) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(requireContext(), "위치 권한이 승인되었습니다", Toast.LENGTH_SHORT).show()
-
-            } else {
-                Toast.makeText(requireContext(), "위치 권한이 거절되었습니다", Toast.LENGTH_SHORT).show()
-
-            }
-        }
-    }
-
-    // GPS가 켜져있는지 확인
-    private fun checkLocationService(): Boolean {
-        val mgr = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return mgr.isProviderEnabled(LocationManager.GPS_PROVIDER)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        binding.deliveryMapMv.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
-    }
 }
