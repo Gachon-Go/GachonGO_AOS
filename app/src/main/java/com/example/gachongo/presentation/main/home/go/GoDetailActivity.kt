ff7c2622677dbd8b61e65ba1ae5e6f45c8db7bb9
tama <readyjoo24@gmail.com>
2023-05-21 18:45:58 +0900

    (ui) 댓글 창 추가

--- app/src/main/java/com/example/gachongo/presentation/main/home/go/GoDetailActivity.kt
+++ app/src/main/java/com/example/gachongo/presentation/main/home/go/GoDetailActivity.kt
@@ -1,8 +1,9 @@
 package com.example.gachongo.presentation.main.home.go
 
 import com.example.gachongo.util.binding.BindingActivity
+import com.example.gachongo_aos.R
 import com.example.gachongo_aos.databinding.ActivityGoDetailBinding
 
-class GoDetailActivity  : BindingActivity<ActivityGoDetailBinding>{
+class GoDetailActivity  : BindingActivity<ActivityGoDetailBinding>(R.layout.activity_go_detail){
 
 }
